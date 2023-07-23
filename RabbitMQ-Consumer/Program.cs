b45ca00ca6faf1f8a6300c68627c3e2974fb67d6
Emirhan Öztürk <68188573+emirhanozturk@users.noreply.github.com>
2023-07-23 16:33:15 +0300

    Add project files.

---
+++ RabbitMQ-Consumer/Program.cs
@@ -0,0 +1,26 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://mewlylyo:HkyxgU8xLIPrVUnhvy3GHnmv9EfgUE-x@woodpecker.rmq.cloudamqp.com/mewlylyo");
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+
+
+channel.QueueDeclare(queue: "queue-example", exclusive: false);
+
+EventingBasicConsumer consumer = new(channel);
+channel.BasicConsume("queue-example", autoAck: true, consumer);
+
+consumer.Received += (sender, ea) =>
+{
+    var body = ea.Body.Span;
+    Console.WriteLine("Received: " + Encoding.UTF8.GetString(body));
+
+};
+
+
+Console.Read();
---
+++ RabbitMQ-Publisher/Program.cs
@@ -0,0 +1,17 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://mewlylyo:HkyxgU8xLIPrVUnhvy3GHnmv9EfgUE-x@woodpecker.rmq.cloudamqp.com/mewlylyo");
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+channel.QueueDeclare(queue: "queue-example", exclusive: false);
+
+byte[] message = Encoding.UTF8.GetBytes("message sended by Publisher");
+channel.BasicPublish(exchange: "", routingKey: "queue-example", body: message);
+
+Console.Read();
